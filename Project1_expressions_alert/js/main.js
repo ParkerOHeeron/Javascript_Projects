3babf126a3205bcf210c77a7e4c187590110d1a2
ParkerOH <parkeroheeron@gmail.com>
2021-01-11 16:10:22 -0800

    Add Project 1

---
+++ Project1_expressions_alert/js/main.js
@@ -0,0 +1,13 @@
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine",
+    Daughter = "Penny", Son = "Zorro"; // creating a rough family object
+var Family = Family.fontcolor("blue"); // Changing all family font-color to blue
+document.write(Dad); // writing to the page the dad variable
+window.alert("Hello World");  //alert style hello world
+document.write(Dad + " and " + Mom);  // concatenated str output
+document.write(3 + 3); // having the page do basic math
+function My_First_Function() {  //Defining a function and naming it
+    var str = "This text is green!";  //Defining a variable and giving it a string value
+    var result = str.fontcolor("green");   //Using the font color method on str variable
+
+    document.getElementById("Green_Text").innerHTML = result;  //Putting the value of result into HTML element with "Green_Text" id
+}
